c269bb9410411066e9394eced669dd7940622063
Dean Wilson <mail@deanwilson.dev>
2026-08-04 17:00:39 +0800

    revert traits in converter

--- include/amanuensis/converter.hpp
+++ include/amanuensis/converter.hpp
@@ -2,44 +2,43 @@
 
 #include "amanuensis/value.hpp"
 #include "amanuensis/json.hpp"
-#include "amanuensis/value-traits.hpp"
 
 namespace Amanuensis {
 
-template <typename TargetValue> class Converter {
+template <typename TargetValue, typename Traits> class Converter {
 public:
   Converter() = delete;
 
   static TargetValue FromValue(const Value& source)
   {
     if (Json::IsNull(source))
-      return ValueTraits<TargetValue>::MakeNull();
+      return Traits::MakeNull();
     if (Json::IsBoolean(source))
-      return ValueTraits<TargetValue>::MakeBoolean(Json::AsBoolean(source));
+      return Traits::MakeBoolean(Json::AsBoolean(source));
     if (Json::IsInteger(source))
-      return ValueTraits<TargetValue>::MakeInteger(Json::AsInteger(source));
+      return Traits::MakeInteger(Json::AsInteger(source));
     if (Json::IsDouble(source))
-      return ValueTraits<TargetValue>::MakeDouble(Json::AsDouble(source));
+      return Traits::MakeDouble(Json::AsDouble(source));
     if (Json::IsString(source))
-      return ValueTraits<TargetValue>::MakeString(Json::AsString(source));
+      return Traits::MakeString(Json::AsString(source));
 
     if (Json::IsArray(source)) {
-      auto target = ValueTraits<TargetValue>::MakeArray();
+      auto target = Traits::MakeArray();
       for (const auto& element : Json::AsArray(source)) {
-        ValueTraits<TargetValue>::PushBack(target, FromValue(element));
+        Traits::PushBack(target, FromValue(element));
       }
       return target;
     }
 
     if (Json::IsObject(source)) {
-      auto target = ValueTraits<TargetValue>::MakeObject();
+      auto target = Traits::MakeObject();
       for (auto it = Json::BeginObject(source); it != Json::EndObject(source); ++it) {
-        ValueTraits<TargetValue>::Insert(target, it->first, FromValue(it->second));
+        Traits::Insert(target, it->first, FromValue(it->second));
       }
       return target;
     }
 
-    return ValueTraits<TargetValue>::MakeNull();
+    return Traits::MakeNull();
   }
 };
 
